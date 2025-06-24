1e63f4d645aef7cd55900084f13a0d13948141c1
ashish707917 <ashish707917@gmail.com>
2025-06-24 18:53:48 +0530

    initial commit

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,29 @@
+
+import Sidebar from './Sidebar'
+import Navbar from './Navbar'
+
+const Layout = ({ children, showSidebar = false }) => {
+  return (
+    <div className="min-h-screen">
+      <div className='flex'>
+   
+       {showSidebar && <Sidebar />}
+
+      <div className="flex-1 flex flex-col">
+   
+        <Navbar/>
+
+        {/* Main content area */}
+        <main className="flex-1 overflow-y-auto">
+          {children}
+        </main>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+
+
+
+export default Layout
